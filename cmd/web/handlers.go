701454c63e890457919bd3a98141f774c3b81c24
Scotty Fionnghall <scottyfionnghall@gmail.com>
2023-09-15 18:27:54 +0300

    added html templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,51 +1,74 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 // Add a "/" handler function
 func home(w http.ResponseWriter, r *http.Request) {
-	if methodAllowed(w, r, http.MethodGet) {
-		// Check if the current request URL path exactly matches "/".
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
-		w.Write([]byte("Snippet home page..."))
+	// Check if the current request URL path exactly matches "/".
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/pages/home.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
 	}
 
 }
 
 // Add a SnippetView handler function.
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	if methodAllowed(w, r, http.MethodGet) {
-		id, err := strconv.Atoi(r.URL.Query().Get("id"))
-		if err != nil || id < 1 {
-			http.NotFound(w, r)
-			return
-		}
-		fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
 	}
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/pages/view.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base", id)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
 }
 
 // Add a SnippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if methodAllowed(w, r, http.MethodPost) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("Create a new snippet..."))
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
 	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("Create a new snippet..."))
+
 }
 
 // Check if requset is using allowed method
-func methodAllowed(w http.ResponseWriter, r *http.Request, allowedRespone string) bool {
-	if r.Method != allowedRespone {
-		w.Header().Set("Allow", allowedRespone)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return false
-	} else {
-		return true
-	}
-}
