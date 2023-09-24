7e82dc0f2142e1df739454d233bfbdb69c53d94b
Scotty Fionnghall <scottyfionnghall@gmail.com>
2023-09-24 17:28:55 +0300

    added post request tests

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
 	"regexp"
 	"testing"
 	"time"
@@ -23,7 +24,7 @@ type testServer struct {
 
 var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
 
-func extractCSFRToken(t *testing.T, body string) string {
+func extractCSRFToken(t *testing.T, body string) string {
 	matches := csrfTokenRX.FindStringSubmatch(body)
 	if len(matches) < 2 {
 		t.Fatal("no csrf token found in body")
@@ -88,4 +89,20 @@ func newTestApplication(t *testing.T) *application {
 		formDecoder:    formDecoder,
 		sessionManager: sessionManager,
 	}
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, string) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
+
+	return rs.StatusCode, rs.Header, string(body)
 }
--- go.mod
+++ go.mod
@@ -6,12 +6,11 @@ require github.com/go-sql-driver/mysql v1.7.1
 
 require github.com/justinas/alice v1.2.0
 
-require github.com/julienschmidt/httprouter v1.3.0
-
 require (
-	github.com/alexedwards/scs/mysqlstore v0.0.0-20230902070821-95fa2ac9d520 // indirect
-	github.com/alexedwards/scs/v2 v2.5.1 // indirect
-	github.com/go-playground/form/v4 v4.2.1 // indirect
-	github.com/justinas/nosurf v1.1.1 // indirect
-	golang.org/x/crypto v0.13.0 // indirect
+	github.com/alexedwards/scs/mysqlstore v0.0.0-20230902070821-95fa2ac9d520
+	github.com/alexedwards/scs/v2 v2.5.1
+	github.com/go-playground/form/v4 v4.2.1
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/justinas/nosurf v1.1.1
+	golang.org/x/crypto v0.13.0
 )
